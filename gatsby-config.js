77bdc24e57c7117544d96777b0c7a27a74a18217
Gary Zhao <gary.j.zhao@gmail.com>
2018-11-29 23:19:40 -0800

    Added graphql filesystem resolver

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Asana -- Pet Adoption Agency`
   },
-  plugins: [`gatsby-plugin-styled-components`]
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/`
+      }
+    }
+  ]
 };
