2b38aeccaac68c17b4da33e3c1ad0b99b9234c1a
Gary Zhao <gary.j.zhao@gmail.com>
2018-12-04 13:18:27 -0800

    Added zoom effect

--- src/components/DogPicker.js
+++ src/components/DogPicker.js
@@ -11,6 +11,40 @@ const ImageLink = styled.a`
   align-content: center;
   margin-bottom: 16px;
   cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  img {
+    transition: all 1s ease;
+  }
+  &:hover {
+    img {
+      transform: scale(1.1);
+    }
+    .image-overlay {
+      opacity: 1;
+    }
+  }
+  .image-overlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: 0.5s ease;
+    background-color: rgba(0, 0, 0, 0.25);
+    &--icon {
+      color: white;
+      font-size: 20px;
+      font-weight: bold;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
 `;
 
 class DogPicker extends React.Component {
@@ -33,6 +67,9 @@ class DogPicker extends React.Component {
       <>
         <ImageLink onClick={this.toggleLightbox}>
           <img src={this.props.thumbnail} alt="Thumnbnail of Dog Pictures" />
+          <div class="image-overlay">
+            <div className="image-overlay--icon">+</div>
+          </div>
         </ImageLink>
 
         {this.state.isOpen && (
