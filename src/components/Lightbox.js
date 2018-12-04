af13c36d7832273cc900e359d5ba9a5ca1e15ae3
Gary Zhao <gary.j.zhao@gmail.com>
2018-12-03 18:24:41 -0800

    Added download orignial full sized image

--- src/components/Lightbox.js
+++ src/components/Lightbox.js
@@ -12,11 +12,13 @@ const Overlay = styled.div`
   left: 0;
   right: 0;
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   z-index: 1000;
   .image-wrapper {
     padding: 0 15px;
+    /* needs flex */
     @media screen and (max-width: 767px) {
       max-width: 400px;
       max-height: 500px;
@@ -31,6 +33,11 @@ const Overlay = styled.div`
         max-height: 500px;
       }
     }
+    h1 {
+      color: #fff;
+      flex: 0 0 500px;
+      text-align: center;
+    }
   }
 `;
 const CloseButton = styled.div`
@@ -43,6 +50,23 @@ const CloseButton = styled.div`
   cursor: pointer;
 `;
 
+const DownloadButton = styled.a`
+  background-color: #fff;
+  border-radius: 3px;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 14px;
+  font-weight: 500;
+  height: 36px;
+  letter-spacing: 0.5px;
+  line-height: 36px;
+  padding: 0 16px;
+  text-align: center;
+  text-decoration: none;
+  transition: background-color 0.15s, color 0.15s ease-in-out;
+  white-space: nowrap;
+`;
+
 class Lightbox extends React.Component {
   render() {
     return (
@@ -51,9 +75,12 @@ class Lightbox extends React.Component {
         aspectRatio={this.props.aspectRatio}
       >
         <CloseButton>&times;</CloseButton>
-        <div class="image-wrapper">
+        <div className="image-wrapper">
           <img src={this.props.fullImage} alt="Dog" />
         </div>
+        <DownloadButton href={this.props.original} target="_blank">
+          Click to download full sized image
+        </DownloadButton>
       </Overlay>
     );
   }
